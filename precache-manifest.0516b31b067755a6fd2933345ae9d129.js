26cbd68adf93d21a2183ca82026a24b0765151ec
Alex <ovens290375@gmail.com>
2019-07-31 01:30:18 +0300

    Updates

---
+++ precache-manifest.0516b31b067755a6fd2933345ae9d129.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4920e3b1a31e93c425acdc2c530ec7a8",
+    "url": "/myzen/index.html"
+  },
+  {
+    "revision": "b5c7554952ce76606295",
+    "url": "/myzen/static/js/5.272eb520.chunk.js"
+  },
+  {
+    "revision": "eaa1f75fbc3a5dddcbc6",
+    "url": "/myzen/static/js/DashboardPage.5bb7378f.chunk.js"
+  },
+  {
+    "revision": "3353a8fc1268c5f7bbab",
+    "url": "/myzen/static/js/LogoutPage.6b7659ce.chunk.js"
+  },
+  {
+    "revision": "66e2bbdf806bfe22e5cd",
+    "url": "/myzen/static/js/NotFoundPage.35e08065.chunk.js"
+  },
+  {
+    "revision": "7ef4e17bed16549cfc7a",
+    "url": "/myzen/static/js/main.2744aba4.chunk.js"
+  },
+  {
+    "revision": "f7793ce0be623cd1ddb0",
+    "url": "/myzen/static/js/runtime~main.ecfddc80.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.86b7793b91aa08ffdb4012b4a41d7761.js"
+  "/myzen/precache-manifest.0516b31b067755a6fd2933345ae9d129.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myzen/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
