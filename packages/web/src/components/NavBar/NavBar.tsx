9bc6aa6b9f8772d62d9466a00afd2e29d8666481
Alex <ovens290375@gmail.com>
2019-09-01 15:13:36 +0300

    feat(nav-bar): implement nav-bar components structure

--- packages/web/src/components/NavBar/NavBar.tsx
+++ packages/web/src/components/NavBar/NavBar.tsx
@@ -6,32 +6,37 @@ import styled from 'styled-components';
 const NavBar = () => (
   <Bar>
     <Logo dark />
-    <ul>
+    <NavList>
       <li>
         <ItemLink to="/" activeClassName="active" exact>
-          <div></div>
-          <div>Home</div>
+          <span className="icon-add-outline"></span>
+          <div>Getting Started</div>
         </ItemLink>
       </li>
       <li>
         <ItemLink to="/search" activeClassName="active">
-          <div></div>
+          <span className="icon-user-plus"></span>
           <div>Recruit</div>
         </ItemLink>
       </li>
       <li>
         <ItemLink to="/find-a-work" activeClassName="active">
-          <div></div>
+          <span className="icon-search"></span>
           <div>Find a work</div>
         </ItemLink>
       </li>
       <li>
         <ItemLink to="/about" activeClassName="active">
-          <div></div>
+          <span className="icon-information-outline"></span>
           <div>About</div>
         </ItemLink>
       </li>
-    </ul>
+    </NavList>
+    <OptionsList>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </OptionsList>
   </Bar>
 );
 
@@ -40,11 +45,17 @@ const Bar = styled.div`
   height: 100%;
   top: 0;
   left: 0;
-  width: 310px;
+  width: 280px;
   background-color: ${props => props.theme.colors.lightGrey};
   padding-left: 20px;
   padding-top: 20px;
   box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+`;
+
+const NavList = styled.ul`
+  margin-top: 60px;
 `;
 
 const ItemLink = styled(NavLink)`
@@ -52,11 +63,21 @@ const ItemLink = styled(NavLink)`
   align-items: center;
   color: ${props => props.theme.colors.darkGrey};
   font-size: 15px;
-  margin-top: 10px;
+  margin-top: 26px;
+  margin-bottom: 26px;
 
   &.active {
     color: ${props => props.theme.colors.primaryBlue};
   }
+
+  span {
+    margin-right: 10px;
+    font-size: 18px;
+  }
+`;
+
+const OptionsList = styled.ul`
+  margin-top: auto;
 `;
 
 export default NavBar;
