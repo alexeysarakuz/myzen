6977ff4795006b9ce5ce7b7f27e2232b96b0a548
Alex <ovens290375@gmail.com>
2019-09-10 20:10:49 +0300

    feat(dashboard): add search-page result listing components

--- packages/web/src/components/SearchField/SearchField.tsx
+++ packages/web/src/components/SearchField/SearchField.tsx
@@ -1,30 +1,210 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
-const SearchField = () => (
-  <FieldContainer>
-    Search for a freelancer…
-    <Input />
-  </FieldContainer>
-);
+interface TagItem {
+  title: string;
+}
+
+interface SearchFieldProps {
+  defaultTags: TagItem[];
+}
+
+const SearchField = ({ defaultTags }: SearchFieldProps) => {
+  const [focused, changeFocus] = useState(false);
+  console.log(focused);
+
+  const [tags, changeTags] = useState(defaultTags);
+  const [fieldValue, changeFieldValue] = useState('');
+
+  const removeTag = (id: number) => {
+    const newTags = tags.filter((_, index) => index !== id);
+    changeTags(newTags);
+  };
+
+  const addTag = (newTag: string) => {
+    if (newTag.replace(/ /g, '') !== '') {
+      const newTags = [
+        ...tags,
+        {
+          title: newTag,
+        },
+      ];
+      changeTags(newTags);
+      changeFieldValue('');
+    }
+  };
+
+  const trackTagAdding = (e: any) => {
+    if (e.key === 'Enter') {
+      addTag(e.target.value);
+    }
+  };
+
+  const setFocus = () => {
+    changeFocus(true);
+  };
+
+  const removeFocus = () => {
+    changeFocus(false);
+  };
+
+  return (
+    <FieldContainer>
+      <FieldContent>
+        {tags.length === 0 && fieldValue.length === 0 && (
+          <Placeholder>Search for a freelancer…</Placeholder>
+        )}
+        <Tags>
+          {tags.map((item: TagItem, index: number) => (
+            <Tag key={item.title}>
+              <div>{item.title}</div>
+              <RemoveTagButton onClick={() => removeTag(index)}>
+                <span className="icon-close"></span>
+              </RemoveTagButton>
+            </Tag>
+          ))}
+        </Tags>
+        <Field
+          value={fieldValue}
+          size={fieldValue.length * 2}
+          onChange={e => changeFieldValue(e.target.value)}
+          onFocus={setFocus}
+          onBlur={removeFocus}
+          onKeyDown={trackTagAdding}
+        />
+      </FieldContent>
+      <SearchButton>
+        <span className="icon-search"></span>
+      </SearchButton>
+    </FieldContainer>
+  );
+};
 
 const FieldContainer = styled.div`
-  width: 644px;
-  max-width: 100%;
-  font-family: 'Lato', cursive;
-  font-size: 14px;
-  font-weight: 400;
-  font-style: italic;
-  color: #878da6;
-  background-color: ${props => props.theme.colors.darkGraySearchFieldBg};
+  display: flex;
+  align-items: center;
+  background-color: ${props => props.theme.colors.white};
   border-radius: 6px;
-  padding: 15px 10px;
-  padding-left: 20px;
+  overflow: hidden;
 `;
 
-const Input = styled.input`
-  background-color: ${props => props.theme.colors.darkGraySearchFieldBg};
-  border: none;
+const FieldContent = styled.label`
+  display: flex;
+  align-items: stretch;
+  box-sizing: border-box;
+  width: 600px;
+  height: 50px;
+  padding: 8px 0;
+  padding-left: 13px;
+  overflow-x: scroll;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
 `;
+
+const Placeholder = styled.div`
+  position: absolute;
+  z-index: 2;
+  font-family: 'Lato', cursive;
+  color: ${props => props.theme.colors.darkGrayPlaceholder};
+  font-style: italic;
+  line-height: 34px;
+  margin-left: 4px;
+`;
+
+const Field = styled.input`
+  position: relative;
+  z-index: 3;
+  height: 34px;
+  font-size: 14px;
+  font-weight: 400;
+  color: ${props => props.theme.colors.primaryBlue};
+  box-sizing: border-box;
+  background-color: transparent;
+  border: none;
+  flex-shrink: 0;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+const Tags = styled.div`
+  display: flex;
+  align-items: stretch;
+  justify-content: flex-start;
+  flex-shrink: 0;
+`;
+
+const Tag = styled.div`
+  display: flex;
+  flex-shrink: 0;
+  align-items: center;
+  font-size: 14px;
+  line-height: 30px;
+  color: ${props => props.theme.colors.white};
+  padding: 0 10px 0 15px;
+  background-color: ${props => props.theme.colors.primaryLightBlueTags};
+  border-radius: 5px;
+  margin-right: 10px;
+  transition: 0.3s;
+  border: 1px solid ${props => props.theme.colors.primaryLightBlueTags};
+
+  &:hover {
+    background-color: transparent;
+    color: ${props => props.theme.colors.primaryLightBlueTags};
+  }
+`;
+
+const RemoveTagButton = styled.div`
+  height: 20px;
+  width: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-left: 15px;
+  border-radius: 3px;
+  color: ${props => props.theme.colors.white};
+  background-color: ${props => props.theme.colors.primaryLightBlueRemoveButton};
+  cursor: pointer;
+  transition: 0.3s;
+
+  &:hover {
+    background-color: ${props =>
+      props.theme.colors.primaryLightBlueRemoveButtonHover};
+  }
+`;
+
+const SearchButton = styled.button`
+  width: 30px;
+  height: 30px;
+  background-color: ${props => props.theme.colors.white};
+  border: none;
+  box-shadow: none;
+  font-size: 25px;
+  margin-right: 10px;
+  margin-left: 10px;
+  color: ${props => props.theme.colors.primaryBlue};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+SearchField.defaultProps = {
+  defaultTags: [
+    {
+      title: 'Voice Artist1',
+    },
+    {
+      title: 'Donald Trump',
+    },
+    {
+      title: 'Airubs',
+    },
+  ],
+};
 
 export default SearchField;
--- packages/web/src/theme/patterns/colors.ts
+++ packages/web/src/theme/patterns/colors.ts
@@ -4,18 +4,41 @@ export const colorScheme = {
   white: { value: '#FFF', name: 'White' },
   black: { value: '#000', name: 'Black' },
   primaryBlue: { value: '#3D4CA5', name: 'PrimaryBlue' },
+  primaryBlueShadow: {
+    value: 'rgba(218, 223, 241, 0.6)',
+    name: 'primaryBlueShadow',
+  },
   primaryLightBlue: {
     value: 'rgba(62, 79, 161, 0.6)',
     name: 'PrimaryLightBlue',
+  },
+  primaryLightBlueTags: {
+    value: '#455CC7',
+    name: 'PrimaryLightBlue',
+  },
+  primaryLightBlueRemoveButton: {
+    value: '#697BD1',
+    name: 'PrimaryLightBlueRemoveButton',
+  },
+  primaryLightBlueRemoveButtonHover: {
+    value: '#3c52c3',
+    name: 'PrimaryLightBlueRemoveButtonHover',
   },
   primaryDarkBlue: {
     value: '#2433A0',
     name: 'PrimaryDarkBlue',
   },
-  primaryDarkGray: { value: '#212121', name: 'PrimaryDarkGray' },
+
+  lightGray: { value: 'rgba(240, 240, 244, 0.75)', name: 'LightGray' },
   primaryLightGray: { value: '#A0A0A0', name: 'PrimaryLightGray' },
+  lightGrayTags: { value: '#F4F5F9', name: 'LightGrayTags' },
+  darkGray: { value: '#939393', name: 'DarkGray' },
+  darkGrayTags: { value: '#7E86A4', name: 'DarkGrayTags' },
+  primaryDarkGray: { value: '#212121', name: 'PrimaryDarkGray' },
   secondaryDarkGray: { value: '#2B3641', name: 'secondaryDarkGray' },
+  darkGrayPlaceholder: { value: '#878DA6', name: 'DarkGrayPlaceholder' },
   primaryGreen: { value: '#5EC695', name: 'PrimaryGreen' },
+  greenRating: { value: '#4ADBBD', name: 'GreenRating' },
   loginPageInputBorder: { value: '#C9C9C9', name: 'LoginPageInputBorder' },
   loginPageInputPlaceholder: {
     value: '#BDBDBD',
@@ -28,9 +51,6 @@ export const colorScheme = {
   cyan: { value: '#366DF9', name: 'Cyan' },
   lightRed: { value: '#FFCBC4', name: 'LightRed' },
   darkRed: { value: '#FF6650', name: 'DarkRed' },
-  lightGray: { value: '#F9F9F9', name: 'LightGray' },
-  darkGray: { value: '#939393', name: 'DarkGray' },
-  darkGraySearchFieldBg: { value: '#F4F4F7', name: 'DarkGraySearchFieldBg' },
 };
 
 export const getColor = (colorKey: string) =>
