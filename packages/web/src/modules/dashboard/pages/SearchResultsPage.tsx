1d4402d509e9db81735a624d929ff82e5062ca0b
Alex <ovens290375@gmail.com>
2019-09-17 20:02:59 +0300

    feat(dashboard): fix styles on wide screens

--- packages/web/src/modules/dashboard/pages/SearchResultsPage.tsx
+++ packages/web/src/modules/dashboard/pages/SearchResultsPage.tsx
@@ -3,10 +3,11 @@ import Profile from 'components/Profile/Profile';
 import React from 'react';
 import { ResultItem } from '../dashboard.models';
 import SearchField from 'components/SearchField/SearchField';
+import SearchResultsItem from 'components/SearchResultsItem/SearchResultsItem';
 import styled from 'styled-components';
 
 const SearchResultsPage = ({ results }: { results: ResultItem[] }) => (
-  <>
+  <Page>
     <NavBar />
     <ResultsContent>
       <ResultsHeader>
@@ -18,62 +19,30 @@ const SearchResultsPage = ({ results }: { results: ResultItem[] }) => (
       </FiltersPanel>
       <ResultsListing>
         {results.map((item: ResultItem) => (
-          <Item key={item.id}>
-            <UserData>
-              <Image src={item.profilePicture} />
-              <>
-                <Name>{item.username}</Name>
-              </>
-            </UserData>
-            <Rating>
-              <RatingNumber>{item.rating} Rating</RatingNumber>
-              <RatingLineContainer>
-                <RatingLine width={item.rating * 10} />
-              </RatingLineContainer>
-            </Rating>
-
-            <TagsTitle>Roles</TagsTitle>
-            <Tags>
-              {item.tags.map(tag => (
-                <Tag key={tag.title}>{tag.title}</Tag>
-              ))}
-            </Tags>
-
-            <ReviewsTitle>
-              REVIEWS ABOUT <ReviewsName>{item.username}</ReviewsName>
-            </ReviewsTitle>
-            <Reviews>
-              <Review>
-                <ReviewImage src={item.profilePicture} />
-              </Review>
-              <Review>
-                <ReviewImage src={item.profilePicture} />
-              </Review>
-              <Review>
-                <ReviewImage src={item.profilePicture} />
-              </Review>
-              <Review>
-                <ReviewImage src={item.profilePicture} />
-              </Review>
-            </Reviews>
-
-            <ButtonsRow>
-              <CommunicateButton>Book now</CommunicateButton>
-              <CommunicateButton>Text now</CommunicateButton>
-              <CommunicateFullButton>Text now</CommunicateFullButton>
-            </ButtonsRow>
-          </Item>
+          <SearchResultsItem item={item} key={item.id} />
         ))}
       </ResultsListing>
     </ResultsContent>
-  </>
+  </Page>
 );
+
+const Page = styled.div`
+  background: ${props => props.theme.colors.lightGray};
+`;
 
 const ResultsContent = styled.div`
   min-height: 100vh;
-  margin-left: 280px;
-  background: ${props => props.theme.colors.lightGray};
-  box-sizing: border-box;
+  padding-left: 280px;
+
+  @media (min-width: 1555px) {
+    padding-left: 310px;
+    padding-right: 30px;
+  }
+
+  @media (min-width: 1655px) {
+    max-width: 1430px;
+    margin: 0 auto;
+  }
 `;
 
 const ResultsHeader = styled.div`
@@ -102,172 +71,10 @@ const ResultsListing = styled.div`
   justify-content: space-around;
   align-items: stretch;
   padding: 15px 30px;
-`;
 
-const Item = styled.div`
-  box-sizing: border-box;
-  padding: 15px;
-  margin-bottom: 15px;
-  width: 23.5%;
-  background-color: ${props => props.theme.colors.white};
-  box-shadow: 0 0 20px ${props => props.theme.colors.primaryBlueShadow};
-`;
-
-const UserData = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-const Image = styled.img`
-  width: 50px;
-  height: 50px;
-  border-radius: 10px;
-`;
-
-const Name = styled.h3`
-  font-weight: 400;
-  font-size: 18px;
-  margin-left: 15px;
-`;
-
-const Rating = styled.div`
-  display: flex;
-  align-items: center;
-  margin-top: 20px;
-  margin-bottom: 30px;
-`;
-
-const RatingNumber = styled.div`
-  width: 35%;
-  font-size: 14px;
-`;
-
-const RatingLineContainer = styled.div`
-  width: 65%;
-`;
-
-const RatingLine = styled.div<{ width: number }>`
-  position: relative;
-  height: 5px;
-  width: 100%;
-  background-color: ${props => props.theme.colors.lightGrayTags};
-  border-radius: 20px;
-  overflow: hidden;
-
-  &::after {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: ${props => props.width}%;
-    height: 100%;
-    background-color: ${props => props.theme.colors.greenRating};
+  @media (min-width: 1515px) {
+    justify-content: space-between;
   }
-`;
-
-const Tags = styled.div`
-  display: flex;
-  align-items: stretch;
-  flex-wrap: wrap;
-`;
-
-const TagsTitle = styled.h3`
-  margin-bottom: 4px;
-  font-size: 13px;
-  font-weight: bold;
-  color: ${props => props.theme.colors.darkGrayTags};
-  text-transform: uppercase;
-`;
-
-const Tag = styled.div`
-  padding: 9px 13px;
-  margin-right: 10px;
-  margin-top: 8px;
-  font-size: 13px;
-  color: ${props => props.theme.colors.darkGrayTags};
-  background-color: ${props => props.theme.colors.lightGrayTags};
-  border-radius: 4px;
-`;
-
-const Reviews = styled.div`
-  display: flex;
-`;
-
-const ReviewsTitle = styled.h3`
-  margin-top: 20px;
-  margin-bottom: 4px;
-  font-size: 13px;
-  font-weight: 600;
-  color: ${props => props.theme.colors.darkGrayTags};
-  text-transform: uppercase;
-`;
-
-const Review = styled.div`
-  margin-top: 10px;
-  position: relative;
-
-  &:last-child {
-    width: 32px;
-    height: 32px;
-
-    &::after {
-      content: '';
-      position: absolute;
-      width: 28px;
-      height: 28px;
-      border-radius: 50%;
-      opacity: 0.6;
-      background-color: ${props => props.theme.colors.black};
-      left: 2px;
-      top: 2px;
-    }
-
-    &::before {
-      content: '+6';
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      transform: translateX(-50%) translateY(-50%);
-      font-size: 10px;
-      color: ${props => props.theme.colors.white};
-      z-index: 5;
-    }
-  }
-`;
-
-const ReviewImage = styled.img`
-  width: 28px;
-  height: 28px;
-  border-radius: 50%;
-  margin-right: -15px;
-  border: 2px solid ${props => props.theme.colors.white};
-`;
-
-const ReviewsName = styled.b`
-  color: ${props => props.theme.colors.primaryBlue};
-`;
-
-const ButtonsRow = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  margin-top: 10px;
-`;
-
-const CommunicateButton = styled.button`
-  padding: 13px;
-  width: 48%;
-  font-weight: 500;
-  margin-top: 10px;
-  color: ${props => props.theme.colors.primaryBlue};
-  text-transform: uppercase;
-  border: 1.5px solid ${props => props.theme.colors.lightGray};
-  border-radius: 5px;
-  background-color: ${props => props.theme.colors.white};
-`;
-
-const CommunicateFullButton = styled(CommunicateButton)`
-  width: 100%;
 `;
 
 SearchResultsPage.defaultProps = {
